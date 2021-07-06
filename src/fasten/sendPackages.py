fd081956b5599cf45b61e8d814e3707698b3aa68
amphioxis <martin.pauling@endocode.com>
2021-07-07 01:07:01 +0200

    Receive Call Graphs from FASTEN for known packages.

--- src/fasten/sendPackages.py
+++ src/fasten/sendPackages.py
@@ -1,13 +1,36 @@
 # Send package name and version to FASTEN and receive Call Graph for it
+
 import json
+import time
+import requests
 
 class SendPackages:
 
     @staticmethod
-    def sendPackages(pkgs):
+    def sendPackages(pkgs, url):
 
         print("In function sendPackages:")
         pkgs = json.loads(pkgs)
 
         for package in pkgs:
-            print(package + " " + pkgs[package])
+
+            URL = url + "/mvn/packages/" + package + "/" + pkgs[package] + "/callgraph"
+
+            try:
+                response = requests.get(url=URL) # Get Call Graph for specified package
+                cg = response.json() # save Call Graph as JSON format
+
+                print(cg)
+
+                if response.status_code == 500:
+                    print("Call Graph for package " + package + " not available!")
+                elif response.status_code != 200:
+                    print("Something went wrong")
+
+            except requests.exceptions.ReadTimeout:
+                print('Connection timeout: ReadTimeout')
+            except requests.exceptions.ConnectTimeout:
+                print('Connection timeout: ConnectTimeout')
+            except requests.exceptions.ConnectionError:
+                print('Connection timeout: ConnectError')
+                time.sleep(30)
