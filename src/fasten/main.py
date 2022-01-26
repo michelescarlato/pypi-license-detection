edf6e070bc86d7208fbfd1552f9668e6a7fde017
amphioxis <martin.pauling@endocode.com>
2022-01-26 21:17:37 +0100

    Use 'argparse' to read in command line arguments.

--- src/fasten/main.py
+++ src/fasten/main.py
@@ -10,16 +10,17 @@ from fasten import FastenPackage
 class Main:
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("call_graph", nargs="*",help="Paths to Call Graph files")
+    parser.add_argument("--product", type=str, help="Package name")
+    parser.add_argument("--pkg_name", type=str, help="Package containing the code to be analyzed")
+    parser.add_argument("--project_path", type=str, help="Path to package to be analyzed")
+    parser.add_argument("--timestamp", type=int, help="Timestamp of the package's version")
+    parser.add_argument("--version", type=str, help="Version of the product")
+    parser.add_argument("--requirements", type=str, help="Path to the requirements file")
+    args = parser.parse_args()
+
     url = 'http://127.0.0.1:9080' # URL to the FASTEN API
-    product = "fasten-pypi-plugin" # Package name
     forge = "local" # Source the product was downloaded from
-    pkg_name = "fasten-pypi-plugin" # Package containing the code to be analyzed
-    version = "1.0" # Version of the product
-    timestamp = "42" # Timestamp of the package's version
-    requirements = '../../requirements.txt' # Path to the requirements.txt file of the project to be analyzed
-    project_path = "./" # Path to project to be analyzed
-    packages = ReadRequirementsFile(requirements)
+    packages = ReadRequirementsFile(args.requirements)
     pkgs = packages.readFile() # read requirements.txt
     max_iter = -1 # Maximum number of iterations through source code (from pycg).
     operation = "call-graph" # or key-error for key error detection on dictionaries (from pycg).
@@ -29,14 +30,13 @@ class Main:
 #    result = package.get_pkg_metadata()
 #    print(result)
 #    call_graphs = ReceiveCallGraphs.receiveCallGraphs(pkgs, url)
-#    call_graphs = CreateCallGraph().createCallGraph(pkg_name, product, forge, version, timestamp, call_graphs, project_path, max_iter, operation)
+#    call_graphs = CreateCallGraph().createCallGraph(args.pkg_name, args.product, forge, args.version, args.timestamp, call_graphs, args.project_path, max_iter, operation)
 #    pathsToCallGraphs = parser.parse_args(call_graphs)
 
     call_graphs = []
     call_graphs.append("./callGraphs/cryptography-3.4.7.json")
     call_graphs.append("./callGraphs/fabric-2.6.0.json")
-    pathsToCallGraphs = parser.parse_args(call_graphs)
 
-    call_graphs = CreateCallGraph().createCallGraph(pkg_name, product, forge, version, timestamp, call_graphs, project_path, max_iter, operation)
+    call_graphs = CreateCallGraph().createCallGraph(args.pkg_name, args.product, forge, args.version, args.timestamp, call_graphs, args.project_path, max_iter, operation)
 
-    StitchCallGraph().stitchCallGraph(pathsToCallGraphs.call_graph)
+    StitchCallGraph().stitchCallGraph(call_graphs)
