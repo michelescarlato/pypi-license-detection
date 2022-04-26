0e687bae0dcbe9a20bca2a4284d3b8dd7e3d5289
amphioxis <martin.pauling@endocode.com>
2022-04-27 01:26:34 +0200

    Request fasten for Call Graphs and vulnerability information.

--- src/fasten/__main__.py
+++ src/fasten/__main__.py
@@ -1,25 +1,26 @@
 import argparse
-
+import sys
 from readRequirementsFile import ReadRequirementsFile
 from checkPackageAvailability import CheckPackageAvailability
 from createCallGraph import CreateCallGraph
 from receiveCallGraphs import ReceiveCallGraphs
+from requestFasten import RequestFasten
 from stitchCallGraph import StitchCallGraph
 from executePypiResolver import ExecutePypiResolver
 from pypiPluginUtils import PypiPluginUtils
 
-def main():
 
+class Main:
 
-    parser = argparse.ArgumentParser(prog='PyPI-plugin')
-    parser.add_argument("--product", type=str, help="Package name") # pypiPlugin-test-online
-    parser.add_argument("--pkg_name", type=str, help="Package containing the code to be analyzed") # pypiPlugin-test-online
-    parser.add_argument("--project_path", type=str, help="Path to package to be analyzed") # /mnt/stuff/projects/work/pypi-plugin/src/fasten/
-    parser.add_argument("--timestamp", type=int, help="Timestamp of the package's version") # 42
-    parser.add_argument("--version", type=str, help="Version of the product") # 1.0
-    parser.add_argument("--requirements", type=str, help="Path to the requirements file") # /mnt/stuff/projects/work/pypi-plugin/requirements.txt
-    parser.add_argument("--fasten_data", type=str, help="Path where the Call Graphs will be stored")
-    parser.add_argument("--scg_path", type=str, help="Path where the Stitched Call Graph will be stored")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--product", type=str, help="Package name")
+    parser.add_argument("--pkg_name", type=str, help="Package containing the code to be analyzed")
+    parser.add_argument("--project_path", type=str, help="Path to package to be analyzed")
+    parser.add_argument("--timestamp", type=int, help="Timestamp of the package's version")
+    parser.add_argument("--version", type=str, help="Version of the product")
+    parser.add_argument("--requirements", type=str, help="Path to the requirements file")
+    parser.add_argument("--cg_path", type=str, help="Path to the Call Graphs to be stored")
+    parser.add_argument("--scg_path", type=str, help="Path to the Stitched Call Graph to be stored")
     args = parser.parse_args()
     PypiPluginUtils.DirectoryCheck(args.fasten_data, args.scg_path)
     DependenciesTree = ExecutePypiResolver.executePypiResolver(args.requirements)
@@ -29,20 +30,16 @@ def main():
     max_iter = -1 # Maximum number of iterations through source code (from pycg).
     operation = "call-graph" # or key-error for key error detection on dictionaries (from pycg).
     call_graphs = []
+    vulnerabilities = []
 
     pkgs = ReadRequirementsFile.readFile(args.requirements) # Read requirements.txt
     pkgs, unknown_pkgs = CheckPackageAvailability.checkPackageAvailability(pkgs, url) # Check if packages are known by FASTEN
 
-# TODO: Enable plugin to receive Call Graphs and metadata information from FASTEN as soon as the pypi-API is ready
-#    package = FastenPackage(url, forge, pkg_name, pkg_version)
-#    result = package.get_pkg_metadata()
-#    print(result)
-    call_graphs = ReceiveCallGraphs.receiveCallGraphs(DependenciesTree, pkgs, url)
 
+    call_graphs = RequestFasten.requestFasten(DependenciesTree, pkgs, url, "rcg")
     call_graphs = CreateCallGraph().createCallGraph(args, forge, max_iter, operation, call_graphs)
+    vulnerabilities = RequestFasten.requestFasten(pkgs, url, "vulnerabilities")
+
 #    pathsToCallGraphs = parser.parse_args(call_graphs)
 
     StitchCallGraph().stitchCallGraph(args, call_graphs)
-
-if __name__ == "__main__":
-    main()
