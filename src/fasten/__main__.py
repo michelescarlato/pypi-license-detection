b74926d3afeeff699ae3c10e66d1f139e3ebb8fb
Michele Scarlato <michele.scarlato@gmail.com>
2022-05-31 18:01:24 +0200

    adding a timer

--- src/fasten/__main__.py
+++ src/fasten/__main__.py
@@ -1,6 +1,6 @@
 import argparse
 import json
-
+import time
 from readRequirementsFile import ReadRequirementsFile
 from checkPackageAvailability import CheckPackageAvailability
 from createCallGraph import CreateCallGraph
@@ -38,7 +38,7 @@ def main():
 
     CreateDirectories.DirectoryCheck(args.fasten_data, args.scg_path) # Create directories to store the Call Graphs and the Stitched Call Graph
     DependenciesTree = ExecutePypiResolver.executePypiResolver(args.requirements)
-
+    time.sleep(10)
     all_pkgs = ReadRequirementsFile.readFile(DependenciesTree) # Read requirements.txt
     pkgs, unknown_pkgs = CheckPackageAvailability.checkPackageAvailability(all_pkgs, url) # Check if packages are known by FASTEN
 
