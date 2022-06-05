141785d296ab8e1aeee120b51364e6c8743eae34
Michele Scarlato <michele.scarlato@gmail.com>
2022-06-05 06:20:35 +0200

    Creating a dictionary with mockup license information

--- src/fasten/requestFastenKnownAndUnknownListsMockup.py
+++ src/fasten/requestFastenKnownAndUnknownListsMockup.py
@@ -3,11 +3,12 @@
 import json
 import time
 import requests
+from gitHubAndPyPIParsingUtils import IsAnSPDX, ConvertToSPDX
 
-class RequestFastenKnownAndUnknownLists:
+class RequestFastenKnownAndUnknownListsMockup:
 
     @staticmethod
-    def requestFastenKnownAndUnknownLists(args, pkgs, url, path):
+    def requestFastenKnownAndUnknownListsMockup(args, pkgs, url, path, LCVurl):
 
         print("Receive " + path + " from FASTEN:")
         pkgs = json.loads(pkgs)
@@ -15,39 +16,88 @@ class RequestFastenKnownAndUnknownLists:
         known_pkg_metadata = {}
         unknown_pkg_metadata = {}
         connectivity_issues = {}
-
+        licenses = {}
+        print(pkgs)
+        i = 0
         for package in pkgs:
+            print(package)
+            f = open("fasten-restapi-mockup/"+path+".json")# save in JSON format
+            packageVersion = pkgs[package]
+            packageName = package
+            metadata_JSON = json.load(f)
+            #print(metadata_JSON)
 
-            URL = url + "packages/" + package + "/" + pkgs[package] + "/" + path
-            #print(URL)
-            try:
-                response = requests.get(url=URL) # get Call Graph or metadata for specified package
+            if path == "metadata":
+                if metadata_JSON["package_name"] == package:
+                    if "licenses" in metadata_JSON["metadata"]:
+                        licenses[i] = {}
+                        list = metadata_JSON["metadata"]["licenses"]
+                        for element in list:
+                            print(element)
+                            if element["source"] == "GITHUB":
+                                IsSPDX = IsAnSPDX(str(element["name"]), LCVurl)
+                                if IsSPDX is not True:
+                                    License = ConvertToSPDX(str(element["name"]), LCVurl)
+                                    if IsAnSPDX(License, LCVurl):
+                                        LicenseSPDX = License
+                                    else:
+                                        licenses[i]['packageName'] = packageName
+                                        licenses[i]['packageVersion'] = packageVersion
+                                        licenses[i]['GitHubLicense'] = str(element["name"])
+                                        pass
 
-                if response.status_code == 200:
+                                    if "LicenseSPDX" in locals():
+                                        # add to licenses the license under the GitHubSPDX key
+                                        licenses[i]['packageName'] = packageName
+                                        licenses[i]['packageVersion'] = packageVersion
+                                        licenses[i]['GitHubLicenseSPDX'] = LicenseSPDX
+                                        pass
+                                else:
+                                    License = str(element["name"])
+                                    licenses[i]['packageName'] = packageName
+                                    licenses[i]['packageVersion'] = packageVersion
+                                    licenses[i]['GitHubLicenseSPDX'] = License
+                                    pass
+                                    # add to licenses the license under the GitHubSPDX key
+                            if element["source"] == "PYPI":
+                                IsSPDX = IsAnSPDX(str(element["name"]), LCVurl)
+                                if IsSPDX is not True:
+                                    License = ConvertToSPDX(str(element["name"]), LCVurl)
+                                    if IsAnSPDX(License, LCVurl):
+                                        LicenseSPDX = License
+                                    else:
+                                        licenses[i]['packageName'] = packageName
+                                        licenses[i]['packageVersion'] = packageVersion
+                                        licenses[i]['PyPI_not_SPDX'] = str(element["name"])
+                                        pass
 
-                    metadata_JSON = response.json() # save in JSON format
-                    with open(args.fasten_data + package + "." + path + ".json", "w") as f:
-                        f.write(json.dumps(metadata_JSON)) # save Call Graph or metadata in a file
+                                if "LicenseSPDX" in locals():
+                                    # add to licenses the license under the GitHubSPDX key
+                                    licenses[i]['packageName'] = packageName
+                                    licenses[i]['packageVersion'] = packageVersion
+                                    licenses[i]['PyPILicenseSPDX'] = LicenseSPDX
+                                    pass
+                                else:
+                                    License = str(element["name"])
+                                    licenses[i]['packageName'] = packageName
+                                    licenses[i]['packageVersion'] = packageVersion
+                                    licenses[i]['PyPILicenseSPDX'] = License
+                                    pass
 
-                    print(type(metadata_JSON))
-                    metadata_JSON_File_Locations.append(args.fasten_data + package + "." + path + ".json") # append Call Graph or metadata file location to a list
+                            with open(args.fasten_data + package + "." + path + ".json", "w") as f:
+                                f.write(json.dumps(metadata_JSON)) # save Call Graph or metadata in a file
+                            metadata_JSON_File_Locations.append(args.fasten_data + package + "." + path + ".json") # append Call Graph or metadata file location to a list
 
-                    print(package + ":" + pkgs[package] + ": " + path + " received.")
-                    known_pkg_metadata[package] = pkgs[package]
+                            print(package + ":" + pkgs[package] + ": " + path + " received.")
+                            known_pkg_metadata[package] = pkgs[package]
+                            i += 1
 
-                elif response.status_code == 500:
-                    print(package + ":" + pkgs[package] + ": " + path + " not available!")
-                    unknown_pkg_metadata[package] = pkgs[package]
                 else:
-                    print("something went wrong")
-                    connectivity_issues[package] = pkgs[package]
+                    print("this package is not in fasten-restapi-mockup")
+                    unknown_pkg_metadata[package] = pkgs[package]
+            if path == "files":
+                for i in metadata_JSON:
+                    if package in metadata_JSON[i]["path"]:
+                        print(metadata_JSON[i]["metadata"])
 
-            except requests.exceptions.ReadTimeout:
-                print('Connection timeout: ReadTimeout')
-            except requests.exceptions.ConnectTimeout:
-                print('Connection timeout: ConnectTimeout')
-            except requests.exceptions.ConnectionError:
-                print('Connection timeout: ConnectError')
-                time.sleep(30)
-
-        return metadata_JSON_File_Locations, known_pkg_metadata, unknown_pkg_metadata, connectivity_issues
+        return licenses, metadata_JSON_File_Locations, known_pkg_metadata, unknown_pkg_metadata, connectivity_issues, i
