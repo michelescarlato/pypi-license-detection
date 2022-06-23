4c2b05817bb36df81689c9c512c59ce4db25db85
Michele Scarlato <michele.scarlato@gmail.com>
2022-06-23 15:46:11 +0200

    Adding cg_producer interface. Local call graph generation in case FASTEN server is not providing call graph for a given package.

--- src/fasten/__main__.py
+++ src/fasten/__main__.py
@@ -1,18 +1,20 @@
 import argparse
 import time
-from fasten.executePypiResolver import ExecutePypiResolver
-from fasten.readRequirementsFile import ReadRequirementsFile
-from fasten.checkPackageAvailability import CheckPackageAvailability
-from fasten.createCallGraph import CreateCallGraph
-from fasten.requestFasten import RequestFasten
-from fasten.stitchCallGraphs import StitchCallGraphs
-from fasten.findEntrypoints import FindEntrypoints
-from fasten.createAdjacencyList import CreateAdjacencyList
-from fasten.depthFirstSearch import DepthFirstSearch
-from fasten.optimizeStitchedCallGraph import OptimizeStitchedCallGraph
-from fasten.enrichCallGraph import EnrichCallGraph
-from fasten.stitchedCallGraphAnalyzer import StitchedCallGraphAnalyzer
-from fasten.createDirectories import CreateDirectories
+from executePypiResolver import ExecutePypiResolver
+from readRequirementsFile import ReadRequirementsFile
+from checkPackageAvailability import CheckPackageAvailability
+from createCallGraph import CreateCallGraph
+from requestFasten import RequestFasten
+from stitchCallGraphs import StitchCallGraphs
+from findEntrypoints import FindEntrypoints
+from createAdjacencyList import CreateAdjacencyList
+from depthFirstSearch import DepthFirstSearch
+from optimizeStitchedCallGraph import OptimizeStitchedCallGraph
+from enrichCallGraph import EnrichCallGraph
+from stitchedCallGraphAnalyzer import StitchedCallGraphAnalyzer
+from createDirectories import CreateDirectories
+from collectingGeneratedAndRetrievedCallGraphs import collectingGeneratedAndRetrievedCallGraphs
+
 
 
 def main():
@@ -44,9 +46,14 @@ def main():
     pkgs = ReadRequirementsFile.readFile(args.requirements) # Read requirements.txt
     pkgs, unknown_pkgs = CheckPackageAvailability.checkPackageAvailability(pkgs, url) # Check if packages are known by FASTEN
 
+    ################################ CALL GRAPHS - Michele - Retrieve and Generation in one function ##################
+    call_graphs = collectingGeneratedAndRetrievedCallGraphs(args, all_pkgs, url)
+    print("call_graphs")
+    print(call_graphs)
 
-    call_graphs, cg_pkgs = RequestFasten.requestFasten(args, pkgs, url, "rcg")
-    call_graphs = CreateCallGraph().createCallGraph(args, forge, max_iter, operation, call_graphs)
+
+    #call_graphs, cg_pkgs = RequestFasten.requestFasten(args, pkgs, url, "rcg")
+    #call_graphs = CreateCallGraph().createCallGraph(args, forge, max_iter, operation, call_graphs)
     vulnerabilities, vul_pkgs = RequestFasten.requestFasten(args, pkgs, url, "vulnerabilities")
 
 #    pathsToCallGraphs = parser.parse_args(call_graphs)
---
+++ src/fasten/executeCallGraphGenerator.py
@@ -0,0 +1,57 @@
+from pycg_producer.producer import CallGraphGenerator
+import time
+import os.path
+import shutil
+
+def deleteCallGraphsDir(CallGraphsDirLocal):
+    if os.path.exists(CallGraphsDirLocal):
+        shutil.rmtree(CallGraphsDirLocal)
+        print("The directory " + CallGraphsDirLocal + " has been deleted successfully")
+        time.sleep(10)
+    else:
+        print("The directory " + CallGraphsDirLocal + " does not exist!")
+
+def executeCallGraphGenerator(unknown_call_graphs, callGraphFastenPath):
+    CallGraphsDirLocal = "directoryName"
+    global CallGraphPaths
+    CallGraphPaths = []
+    for key in unknown_call_graphs:
+        print(key, unknown_call_graphs[key])
+        packageName = key
+        packageVersion = unknown_call_graphs[key]
+
+        coord = { "product": ""+packageName+"",
+              "version": ""+packageVersion+"",
+              "version_timestamp": "2000",
+              "requires_dist": []}
+
+        CallGraphPathLocal = CallGraphsDirLocal + "/" + "callgraphs"+ "/" + packageName[0] + "/" + packageName + "/" + packageVersion + "/" + "cg.json"
+        executeSingleCallGraphGeneration(coord, packageName, packageVersion, CallGraphsDirLocal, CallGraphPathLocal, callGraphFastenPath)
+    return CallGraphPaths
+
+
+def executeSingleCallGraphGeneration(coord, packageName, packageVersion, directoryName, CallGraphPathLocal,callGraphFastenPath):
+
+    generator = CallGraphGenerator(directoryName, coord)
+    print(generator.generate())
+    # wait for the call graph to be generated
+    print("Waiting for call graph generation at: ")
+    print(CallGraphPathLocal)
+    timer = 1
+    global CallGraphPaths
+    CallGraphPaths = []
+    while not os.path.exists(CallGraphPathLocal):
+        time.sleep(1)
+        timer += 1
+        if timer > 30:
+            print(""+str(timer)+" seconds without call graph generation passed.")
+            break
+    if os.path.isfile(CallGraphPathLocal):
+        print("Call graph generated at: "+CallGraphPathLocal)
+        CallGraphPathLocalRenamed = CallGraphPathLocal.replace("cg.json", packageName+"-"+packageVersion+".json")
+        os.rename(CallGraphPathLocal, CallGraphPathLocalRenamed)
+        shutil.copy(CallGraphPathLocalRenamed, callGraphFastenPath )
+        CallGraphPaths.append(callGraphFastenPath+"/"+packageName+"-"+packageVersion+".json")
+        pass
+    else:
+        print("%s has not been generated!" % CallGraphPathLocal)
