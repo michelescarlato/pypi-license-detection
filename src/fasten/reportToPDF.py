b3fcbd86b8a1ffb639f49a87df094eaa6fa423fb
Michele Scarlato <michele.scarlato@gmail.com>
2022-10-07 11:39:32 +0900

    Adding analyzed package name to the report.

--- src/fasten/reportToPDF.py
+++ src/fasten/reportToPDF.py
@@ -3,14 +3,14 @@ from fpdf import FPDF
 class ReportToPDF:
 
     @staticmethod
-    def reportToPDF(report):
+    def reportToPDF(report, packageName):
         """Save the final report of the analysis in a pdf-file."""
 
 
         pdf = FPDF()
         pdf.add_page()
         pdf.set_font("Arial", 'U', size = 15)
-        pdf.cell(200, 10, txt = "Analysis report", ln = 1, align = 'C')
+        pdf.cell(200, 10, txt = "Analysis report: "+packageName, ln = 1, align = 'C')
 
         pdf.set_font("Arial", size = 10)
         pdf.multi_cell(200, 5, txt = report, align = 'C')
