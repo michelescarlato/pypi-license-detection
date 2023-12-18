82a4ded46c7888c423537838cea9a256666f9ab0
Michele Scarlato <michele.scarlato@gmail.com>
2023-12-18 07:15:34 +0100

    Introduced a dependency resolution for licensing, including all packages (skip the package broken list)

--- src/fasten/executePypiResolver.py
+++ src/fasten/executePypiResolver.py
@@ -6,27 +6,47 @@ import pypiResolver
 * SPDX-License-Identifier: Apache-2.0
 '''
 
+
 class ExecutePypiResolver:
 
     @staticmethod
-    def executePypiResolver(requirementsTxt, package_list):
+    def execute_pypi_resolver(requirements_txt, package_list):
         print("Resolve dependencies...")
-        resolved_packages = pypiResolver.run_pip(requirementsTxt, True)
+        resolved_packages = pypiResolver.run_pip(requirements_txt, True)
 
-        broken_list = ["pytest","future","python-heatclient","PyYAML","rcssmin","Click","py","openstacksdk","Django","pyScss","python-neutronclient","Pygments","Pint","cryptography","django_openstack_auth","setuptools","pyinotify","rjsmin","tqdm","fonttools","pycodestyle","pyrsistent","Dj","Pt"]
+        # packages who break the call graph generation
+        broken_list = ["pytest", "future", "python-heatclient", "PyYAML", "rcssmin", "Click",
+                       "py", "openstacksdk", "Django", "pyScss", "python-neutronclient", "Pygments",
+                       "Pint", "cryptography", "django_openstack_auth", "setuptools", "pyinotify",
+                       "rjsmin", "tqdm", "fonttools", "pycodestyle", "pyrsistent", "Dj", "Pt"]
+
         for package in resolved_packages[1]:
-
-            dct =   {   "name": package[0],
-                        "version": package[1],
-                        "cg_file": None,
-                        "vulnerabilities": None,
-                        "callables": None
-                    }
-            package_list.append(dct)
-
+            if package[0] not in broken_list:
+                dct = {"name": package[0],
+                       "version": package[1],
+                       "cg_file": None,
+                       "vulnerabilities": None,
+                       "callables": None}
+                package_list.append(dct)
+            else:
+                print(f'{package[0]} not inserted because pypi skipped from  ')
         for package in package_list:
             for broken in broken_list:
                 if broken == package["name"]:
                     package_list.remove(package)
+        print("Dependencies resolved.")
+        return package_list
+
+    def execute_pypi_resolver_for_licensing(requirements_txt, package_list):
+        print("Resolve dependencies...")
+        resolved_packages = pypiResolver.run_pip(requirements_txt, True)
+
+        for package in resolved_packages[1]:
+            dct = {"name": package[0],
+                   "version": package[1],
+                   "cg_file": None,
+                   "vulnerabilities": None,
+                   "callables": None}
+            package_list.append(dct)
         print("Dependencies resolved.")
         return package_list
